497e4f2a1c7e0c4a58a8038b9057e687aaa01aa3
Nitish Shinde <nitishiphonexr@gmail.com>
2021-02-26 20:00:03 +0530

    project initialized

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Roaster Buster</Text></View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
